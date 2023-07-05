c63061244fad8a6e219ab0d4d19b13297cb634e2
spioneracorei8 <bestza147.aa@gmail.com>
2023-07-05 09:20:42 +0700

    feat: created cor middleware allow client access data from server

--- server/Middlewares/Middleware.go
+++ server/Middlewares/Middleware.go
@@ -1,9 +1,20 @@
 package Middlewares
 
-import (
+import "github.com/gin-gonic/gin"
 
-)
+func Middleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
 
-func Middleware() {
-	
+	}
+}
+
+
+func CorsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Content-Type", "Application/json")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET POST PUT DELETE OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Authorization, X-Custom-Header")
+		c.Next()
+	}
 }
--- server/Routes/Routes.go
+++ server/Routes/Routes.go
@@ -3,15 +3,16 @@ package Routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/spioneracorei8/Cafe-Crafter/CoffeeAPI/ControllersCoffee"
+	"github.com/spioneracorei8/Cafe-Crafter/Middlewares"
 	"github.com/spioneracorei8/Cafe-Crafter/UserAPI/ControllersUsers"
-
 )
 
 func SetRouter() *gin.Engine {
 	r := gin.Default()
 	CoffeeRotes := r.Group("/coffee-api")
 	{
-		CoffeeRotes.GET("/coffee", ControllersCoffee.GetCoffee)
+		// CoffeeRotes.Use()
+		CoffeeRotes.GET("/coffee", Middlewares.Middleware(), Middlewares.CorsMiddleware(), ControllersCoffee.GetCoffee)
 		CoffeeRotes.GET("coffee/:id", ControllersCoffee.GetCoffeeById)
 		CoffeeRotes.POST("/coffee", ControllersCoffee.InsertCoffee)
 		CoffeeRotes.PUT("/coffee/:id", ControllersCoffee.UpdateCoffee)
