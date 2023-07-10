1e9c454dc0a7707839cc70c8a8d1446c7a25c942
spioneracorei8 <bestza147.aa@gmail.com>
2023-07-10 20:31:39 +0700

    feat: use react-router-dom to link each session in home page

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -5,17 +5,35 @@ import Instagram_Icon from "../assets/Icon/Instagram_Icon.svg"
 import Facebook_Icon from "../assets/Icon/Facebook_Icon.svg"
 import Twitch_Icon from "../assets/Icon/Twitch_Icon.svg"
 import Network_Icon from "../assets/Icon/Network_Icon.svg"
+import { Link } from "react-router-dom"
 
 const Footer = () => {
+
+
     return (
-        <footer>
+        <footer id='contact-us'>
             <div className='footer-container'>
                 <img src={Network_Icon} alt="network icon" className='network-icon' />
                 <div className='contact-icon'>
-                    <img src={Twitter_Icon} alt="twitter icon" className='twitter-icon' />
-                    <img src={Instagram_Icon} alt="instagram icon" className='instagram-icon' />
-                    <img src={Facebook_Icon} alt="facebook icon" className='facebook-icon' />
-                    <img src={Twitch_Icon} alt="twitch icon" className='twitch-icon' />
+
+                    <Link to={"/pageNotFound"}>
+                        <img src={Twitter_Icon} a="twitter icon" className='twitter-icon' />
+                    </Link>
+
+                    <Link to={"/pageNotFound"}>
+                        <img src={Instagram_Icon} alt="instagram icon" className='instagram-icon' />
+                    </Link>
+
+                    <Link to={"/pageNotFound"}>
+                        <img src={Facebook_Icon} alt="facebook icon" className='facebook-icon' />
+
+                    </Link>
+
+                    <Link to={"/pageNotFound"}>
+                        <img src={Twitch_Icon} alt="twitch icon" className='twitch-icon' />
+
+                    </Link>
+
                 </div>
                 <div className='copyright'>
                     <p>© 2023 Cafe-Crafter. All Rights Brewserved.</p>
