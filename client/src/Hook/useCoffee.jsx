a5b8ecac8e3244ccff30089f5ce93cdfde07783f
spioneracorei8 <bestza147.aa@gmail.com>
2023-07-28 16:25:19 +0700

    feat: demo show all coffee

--- client/src/Hook/useCoffee.jsx
+++ client/src/Hook/useCoffee.jsx
@@ -6,7 +6,7 @@ const useCoffee = () => {
     const [isLoading, setIsLoading] = useState(false)
     const [isError, setIsError] = useState(false)
     const [suggestCoffee, setSuggestCoffee] = useState([])
-
+    const [allCoffee, setAllCoffee] = useState([])
 
 
     const getSuggestCoffee = async () => {
@@ -15,6 +15,20 @@ const useCoffee = () => {
             setIsLoading(true);
             const result = await axios.get(`http://localhost:4000/suggest-coffee/`)
             setSuggestCoffee(result.data.data)
+            setIsLoading(false)
+        } catch (error) {
+            setIsError(true);
+            setIsLoading(false);
+            console.log(error);
+        }
+    }
+
+    const getAllCoffee = async () => {
+        try {
+            setIsError(false)
+            setIsLoading(true)
+            const result = await axios.get(`http://localhost:4000/coffee/`)
+            setAllCoffee(result.data.data)
             setIsLoading(false)
         } catch (error) {
             setIsError(true);
@@ -35,6 +49,8 @@ const useCoffee = () => {
         isError,
         setIsError,
         suggestCoffee,
+        getAllCoffee,
+        allCoffee,
     }
 }
 
--- client/src/Pages/HomePageLogin.jsx
+++ client/src/Pages/HomePageLogin.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import { useAuth } from '../Context/Authentication'
+import React, { useEffect } from 'react'
 import NavigationbarLogin from '../Components/NavigationbarLogin'
 import './HomePageLogin.css'
+import useCoffee from '../Hook/useCoffee'
 const HomePageLogin = () => {
-    const { logout } = useAuth()
+
+    const { getAllCoffee, allCoffee } = useCoffee()
+
+
+    useEffect(() => {
+        getAllCoffee()
+    }, [])
+
+
     return (
         <>
             <NavigationbarLogin />
@@ -14,6 +22,32 @@ const HomePageLogin = () => {
                     <li>Cake</li>
                 </ul>
             </nav>
+            <div className='menu-container'>
+                <div className='menu-background'>
+                    {allCoffee.map((item, index) => {
+                        return (
+                            <div
+                                className='menu'
+                                key={index}
+                            >
+                                <h3>{item.Name}</h3>
+
+                                <h4>{item.Price}฿</h4>
+                                <img src={item.Image_url} alt="img" />
+
+                                <div className='buy-now-container'>
+                                    <button className='buy-now'>Buy Now</button>
+                                    <button
+                                        className='learn-more'
+                                    >
+                                        Add To Cart
+                                    </button>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
         </>
     )
 }
