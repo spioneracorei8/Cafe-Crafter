893b725d8feab10426a4cb6ef22524eeb0ca44f2
spioneracorei8 <bestza147.aa@gmail.com>
2023-07-28 22:21:46 +0700

    feat: change the right path in the footer section

--- client/src/Pages/AuthenticatedApp.jsx
+++ client/src/Pages/AuthenticatedApp.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import HomePageLogin from './HomePageLogin'
+import PageNotFound from './PageNotFound'
 const AuthenticatedApp = () => {
     return (
         <>
             <Routes>
                 <Route path="/" element={<HomePageLogin />} />
+                <Route path="/Cafe-Crafter-Twitter" element={<PageNotFound />} />
+                <Route path="/Cafe-Crafter-Instagram" element={<PageNotFound />} />
+                <Route path="/Cafe-Crafter-Facebook" element={<PageNotFound />} />
+                <Route path="/Cafe-Crafter-Twitch" element={<PageNotFound />} />
+
             </Routes>
         </>
     )
