bf295a3bb20afce37e04de57d37842b9231eb6d9
spioneracorei8 <bestza147.aa@gmail.com>
2023-09-09 21:51:46 +0700

    created delete menu page in admin successfully.

---
+++ client/src/Pages/Admin/DeleteMenu/DeleteMenu.jsx
@@ -0,0 +1,199 @@
+import React, { useEffect, useState } from 'react'
+import "./DeleteMenu.css"
+import Carousel from 'react-elastic-carousel';
+import useMenus from '../../../Hook/useMenus'
+import axios from 'axios';
+import Cross from "../../../assets/Icon/Cross.png"
+
+const DeleteMenu = ({ category, toggleNavbarLeft }) => {
+    const [isLoading, setIsLoading] = useState(false)
+    const [isError, setIsError] = useState(false)
+
+    const { getAllCoffee, allCoffee, setAllCoffee, getAllTea, allTea, setAllTea } = useMenus()
+
+    const [deleteMenuId, setDeleteMenuId] = useState(0)
+    const [deleteMenuName, setDeleteMenuName] = useState("")
+    const [deleteMenuImageUrl, setDeleteMenuImageUrl] = useState("")
+    const [isDeletePopUp, setIsDeletePopUp] = useState(false)
+
+    const breakPoints = [
+        {
+            width: 1200, itemsToShow: 3,
+        }
+    ]
+
+    if (category === "Coffee") {
+        useEffect(() => {
+            getAllCoffee()
+            setAllTea([])
+        }, [category])
+    } else if (category === "Tea") {
+        useEffect(() => {
+            getAllTea()
+            setAllCoffee([])
+        }, [category])
+    }
+
+    const handleSelectDeleteMenu = async (menuId, category) => {
+        if (category === "Coffee") {
+            try {
+                setIsError(false)
+                setIsLoading(true)
+                const result = await axios.get(`http://localhost:4000/menus/coffee/${menuId}`)
+                setDeleteMenuName(result.data.data?.Name)
+                setDeleteMenuImageUrl(result.data.data?.Image_url)
+                setIsLoading(false)
+            } catch (error) {
+                setIsError(true);
+                setIsLoading(false);
+                console.log(error);
+            }
+        } else if (category === "Tea") {
+            //     try {
+            //         setIsError(false)
+            //         setIsLoading(true)
+            //         const result = await axios.get(`http://localhost:4000/menus/tea/${menuId}`)
+            //         setEditMenuName(result.data.data?.Name)
+            //         setEditMenuPrice(result.data.data?.Price)
+            //         setEditMenuDescription(result.data.data?.Description)
+            //         setEditMenuImageUrl(result.data.data?.Image_url)
+            //         setIsLoading(false)
+            //     } catch (error) {
+            //         setIsError(true);
+            //         setIsLoading(false);
+            //         console.log(error);
+            //     }
+        }
+    }
+
+    const handleConfirmDeleteMenu = (event, catrgory, menuId) => {
+        if (category === "Coffee") {
+
+        }
+    }
+
+    return (
+
+        <>
+
+            {isDeletePopUp &&
+                <div className='menu-popup-container' onClick={() => {
+                    return (
+                        setIsDeletePopUp(false),
+                        setDeleteMenuId(0),
+                        setDeleteMenuName(""),
+                        setDeleteMenuImageUrl("")
+                    )
+                }}>
+                    <div className='menu-popup' onClick={(event) => event.stopPropagation()}>
+                        <div className="delete-popup-content">
+                            <h1>{deleteMenuName}</h1>
+                            <img src={deleteMenuImageUrl} alt={deleteMenuName} />
+                        </div>
+                        <div className='delete-popup-button'>
+                            <button
+                                onClick={(event) => handleConfirmDeleteMenu(event, category, deleteMenuId)}
+                            >
+                                Confirm Delete {deleteMenuName}
+                            </button>
+                            <button
+                                onClick={() => {
+                                    return (
+                                        setIsDeletePopUp(false),
+                                        setDeleteMenuId(0),
+                                        setDeleteMenuName(""),
+                                        setDeleteMenuImageUrl("")
+                                    )
+                                }}
+                            >
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
+                </div >}
+
+
+            <form className='delete-menu-container' onSubmit={(event) => {
+                return (
+                    event.preventDefault(),
+                    setIsDeletePopUp(true)
+                )
+            }}>
+                <div className='delete-menu-heading'>
+                    <h1>
+                        Delete {category}
+                    </h1>
+                    <h2>Select {category} to Delete</h2>
+                </div>
+
+                <div className='menus-container'>
+                    <div className='menus'>
+                        <Carousel
+                            breakPoints={breakPoints}
+                            className={toggleNavbarLeft ? "rec-carousel-wrapper-load" : "rec-carousel-wrapper-unload"}
+                        >
+                            {category === "Coffee"
+                                ? allCoffee.map((item, index) => {
+                                    return (
+                                        <>
+                                            <div className="menus-list-container" key={index}>
+                                                <div className='menus-list'>
+                                                    <h3>
+                                                        {item?.Name}
+                                                    </h3>
+                                                    <img src={item?.Image_url} alt={item?.Name + " Picture"} loading='lazy' onClick={() => {
+                                                        return (
+                                                            handleSelectDeleteMenu(item?.Id, "Coffee"),
+                                                            setDeleteMenuId(item?.Id)
+                                                        )
+                                                    }} />
+                                                </div>
+                                            </div>
+                                        </>
+                                    )
+                                })
+                                : category === "Tea"
+                                    ? allTea.map((item, index) => {
+                                        return (
+                                            <>
+                                                <div className="menus-list-container" key={index}>
+                                                    <div className='menus-list' >
+                                                        <h3>
+                                                            {item?.Name}
+                                                        </h3>
+                                                        <img src={item?.Image_url} alt={item?.Name + " Picture"} loading='lazy' onClick={() => {
+                                                            return (
+                                                                handleSelectDeleteMenu(item?.Id, "Tea"),
+                                                                setDeleteMenuId(item?.Id)
+                                                            )
+                                                        }} />
+                                                    </div>
+                                                </div>
+                                            </>
+                                        )
+                                    })
+                                    : ""
+                            }
+                        </Carousel>
+                    </div>
+                </div>
+
+                <div className='delete-menu-show'>
+                    <h1>Delete {deleteMenuName === "" ? "?" : deleteMenuName}</h1>
+                    <img src={deleteMenuImageUrl} alt={deleteMenuImageUrl === "" ? "?" : deleteMenuName} />
+                </div>
+
+                <div className='submit-delete-menu'>
+                    <button type='submit' >
+                        <h1>
+                            Delete {category}
+                        </h1>
+                    </button>
+                </div>
+
+            </form>
+        </>
+    )
+}
+
+export default DeleteMenu
