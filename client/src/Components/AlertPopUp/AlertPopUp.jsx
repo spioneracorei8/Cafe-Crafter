2085e3292f51dead50815bf4e945a6229236439d
spioneracorei8 <bestza147.aa@gmail.com>
2023-09-15 21:16:41 +0700

    feat: alert popup register page

--- client/src/Components/AlertPopUp/AlertPopUp.jsx
+++ client/src/Components/AlertPopUp/AlertPopUp.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import "./AlertPopUp.css"
 
-const AlertPopUp = () => {
+const AlertPopUp = ({ detailsAlert, headingAlert }) => {
+  console.log(detailsAlert);
+
   return (
     <>
-      <div>
-        {
-          Swal.fire({
-            title: 'Error!',
-            text: 'Do you want to continue',
-            icon: 'error',
-            confirmButtonText: 'Cool'
-          })
-        }
+      <div className='alert-pop-container'>
+        <div className='alert-pop'>
+          <div className='alert-content'>
+            <h1>
+              {headingAlert}
+            </h1>
+            <p>
+              {detailsAlert}
+            </p>
+          </div>
+        </div>
+
       </div>
-    </> 
+    </>
   )
 }
 
